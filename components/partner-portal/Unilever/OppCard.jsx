e0357202f4e6eceecb8b30e4a6f1024cf8a65b7f
Dinuka Avinash <e19029@eng.pdn.ac.lk>
2024-11-24 17:26:16 +0530

    add form link

--- components/partner-portal/Unilever/OppCard.jsx
+++ components/partner-portal/Unilever/OppCard.jsx
@@ -49,7 +49,7 @@ function OppCard({ OppCard }) {
             </div>
             <div className="font-normal text-md pb-2 text-aiesec-dark-grey"></div>
             <div className="mt-1 text-center">
-              <Link href={opportunity.link} target="_blank">
+              <Link href={"https://forms.gle/yhAWjSHna1FUVgzMA"} target="_blank">
                 <Button
                   className="px-2 mx-auto font-bold bg-transparent text-black border border-black hover:scale-105 transition ease-in hover:bg-black hover:text-white"
                   size="sm"
